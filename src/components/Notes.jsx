30131acf1fae4e87897a36810587f67601114ea8
unknown <masudahmed.edu@gmail.com>
2023-06-23 20:06:03 +0300

    masud-ahmed

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,27 +1,20 @@
 /* eslint-disable react/prop-types */
 
-import React, { useEffect } from "react";
+import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-import { useSelector, useDispatch } from "react-redux";
-import { deleteNote, fetchNotes } from "../store/api/NoteSlice";
+ 
+import {   useFechNotesQuery , useDeleteNotesMutation  } from "../store/api/NoteSlice";
 import { Link } from "react-router-dom";
 
 function Notes() {
-  const allNotes = useSelector((state) => state.notes);
-
-  const { notes, status, error } = allNotes;
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchNotes());
-  }, [dispatch]);
-
-  const deleteNoteHandler = (id) => {
-    dispatch(deleteNote(id));
+   const [deleteNotes] = useDeleteNotesMutation();
+   const {data:notes=[] } =  useFechNotesQuery();
+   console.log("data: " ,notes);
+  const deleteNoteHandler = (noteId) => {
+    deleteNotes(noteId);
   };
   
-
+// const notes=[];
   return (
     <div className="flex flex-wrap justify-center mt-5">
       {status === "loading" && <div className="relative p-5 bg-yellow-400 w-64 h-64 m-5 shadow-2xl overflow-hidden">Loading...</div>}
@@ -29,8 +22,7 @@ function Notes() {
       {notes.map((note) =>  (
         <div
           className="relative bg-yellow-400 w-64 h-64 m-5 shadow-2xl overflow-hidden"
-          key={note.id}
-        >
+          key={note.id} >
           <div className="p-5">
             <h3 className="font-bold text-2xl mb-4">{note.title}</h3>
             <p>{note.content}</p>
--- src/store/api/NoteSlice.js
+++ src/store/api/NoteSlice.js
@@ -1,62 +1,65 @@
-import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
-import axios from "axios";
+/// in this RTK version we dont need any axios
+/// sidookale waa cache naga caawinaaya wax xoogaa speed dheeri ah maadaam data dii ay taalo localka
+///step1 waxaan soo import garaysanj doonaa RTK query from redux
+// import React from "react";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const initialState = {
-    notes: [],
-    status: "idle",
-    error: null
-};
+const NoteUrl = "http://localhost:9002";
 
-export const fetchNotes = createAsyncThunk("note/fetchNotes", async () => {
-    const response = await axios.get("http://localhost:9000/notes");
-    return response.data;
+export const NoteSlice = createApi({
+  reducerPath: "noteApi",
+  baseQuery: fetchBaseQuery({
+   baseUrl: NoteUrl,
+  }),
+  tagTypes: ["notes"],
+  endpoints: (builder) => ({
+    fechNotes: builder.query({
+      query: () => {
+        return {
+          url: "/notes",
+          method: "GET",
+        };
+      },
+      providesTags: ["notes"],
+    }),
+
+    addNotes: builder.mutation({
+      query: (newNote) => {
+        return {
+          url: "/create_note",
+          method: "POST",
+          body: newNote,
+        };
+      },
+      invalidatesTags: ["notes"],
+    }),
+
+    editNotes: builder.mutation({
+      query: ({ noteId, updatedNote }) => {
+        return {
+          url: `/update_note/${noteId}`,
+          method: "PUT",
+          body: updatedNote,
+        };
+      },
+      invalidatesTags: ["notes"],
+    }),
+    deleteNotes: builder.mutation({
+      query: ( noteId ) => {
+        return {
+          url: `/delete_note/${noteId}`,
+          method: "DELETE"
+        };
+      },
+      invalidatesTags: ["notes"],
+    }),
+  }),
 });
+export const {
+  useFechNotesQuery,
+  useAddNotesMutation,
+  useEditNotesMutation,
+  useDeleteNotesMutation,
+} = NoteSlice;
 
-export const addNote = createAsyncThunk("note/addNote", async (newNote) => {
-    const response = await axios.post("http://localhost:9000/create_note", newNote);
-    return response.data;
-});
-
-export const editNote = createAsyncThunk("note/editNote", async ({noteId, updateNote}) => {
-    const response = await axios.put(`http://localhost:9000/update_note/${noteId}`, updateNote);
-    return response.data;
-});
-
-export const deleteNote = createAsyncThunk("note/deleteNote", async (noteId) => {
-    await axios.delete(`http://localhost:9000/delete_note/${noteId}`);
-    return noteId;
-});
-
-export const noteSlice = createSlice({
-    name: "notes",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder.addCase(fetchNotes.pending, (state) => {
-            state.status = "loading";
-            state.error = null;
-        }).addCase(fetchNotes.fulfilled, (state, action) => {
-            state.status = "succeeded";
-            state.notes = action.payload;
-        }).addCase(fetchNotes.rejected, (state, action) => {
-            state.status = "failed";
-            state.error = action.error.message;
-        }).addCase(addNote.fulfilled, (state, action) => {
-            state.notes.push(action.payload);
-        }).addCase(editNote.fulfilled, (state, action) => {
-            const {id, title, content} = action.payload;
-            const existingNote = state.notes.find((note) => Number(note.id) === Number(id));
-            if (existingNote) {
-                existingNote.title = title;
-                existingNote.content = content;
-            }
-
-        }).addCase(deleteNote.fulfilled, (state, action) => {
-            const noteId = action.payload;
-            state.notes = state.notes.filter((note) => note.id !== noteId);
-        });
-    }
-});
-
-
-export default noteSlice.reducer;
+ export default NoteSlice.reducer ;
