e1c39901d65f342bbd9f25c038bf5b096fe52583
hafsa-ali <hafsaa.5353@gmail.com>
2023-06-23 21:24:38 -0700

    finished RTK Assignment

--- src/components/EditNote.jsx
+++ src/components/EditNote.jsx
@@ -1,95 +1,85 @@
 import React, { useEffect, useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import { useSelector, useDispatch } from "react-redux";
-import { editNote, fetchNotes } from "../store/api/NoteSlice";
+import {useEditNoteMutation,useFetchNotesQuery } from "../store/api/NoteSlice";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-
 const EditNote = () => {
-
-  const dispatch = useDispatch();
   const params = useParams();
   const navigate = useNavigate();
-
-  const [initialValues, setInitialValues] = useState({
-    title: '',
-    content: '',
+  const [editNote]=useEditNoteMutation()
+  const {data:notes=[]}=useFetchNotesQuery({});
+  const [note, setCurrentNote] = useState({
+    title: "",
+    content: "",
   });
 
-  const allNotes = useSelector((state) => state.notes.notes);
 
   useEffect(() => {
-    dispatch(fetchNotes());
-  }, [dispatch]);
-  
-  useEffect(() => {
-    const note = allNotes.find((note) => note.id === Number(params.id));
+    const note = notes.find(note => note.id === Number(params.id));
     if (note) {
-      setInitialValues({
-        title: note.title,
-        content: note.content,
-      });
+      setCurrentNote(note);
     }
-  }, [allNotes, params.id]);
+  }, [params.id, notes])
 
-
-
+  const initialValues = {
+    title: note.title,
+    content: note.content
+  }
   const validationSchema = Yup.object({
     title: Yup.string().required('Title is required'),
     content: Yup.string().required('Content is required'),
   });
 
-  const handleSubmit = (values) => {
- 
-    dispatch(editNote({
-      noteId: Number(params.id),
-      updateNote: values,
-    })).then(() => {
-      navigate('/');
-    });
+  const handleSubmit = (values,{ resetForm }) => {
+    editNote({
+      id: Number(params.id),
+      updateNote: values
+    }).unwrap().then(()=>{
+      navigate("/")
+    })
+    resetForm();
   };
 
   return (
-    <div className="bg-white p-10 rounded-lg shadow md:w-3/4 mx-auto lg:w-1/2">
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-        enableReinitialize
+    <div className="bg-white p-10 rounded-lg shadow w-[95%]">
+    <Formik
+      enableReinitialize
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
       >
-        <Form>
-          <div className="mb-5">
-            <Field
-              type="text"
-              id="title"
-              name="title"
-              placeholder="Title"
-              className="border border-gray-300 shadow p-3 w-full rounded mb-"
-            />
-            <ErrorMessage name="title" component="div" className="text-red-500" />
-          </div>
+      <Form>
+        <div className="mb-5">
+          <Field
+            type="text"
+            id="title"
+            name="title"
+            placeholder="Title"
+            className="border border-gray-300 shadow p-3 w-full rounded mb-"
+          />
+          <ErrorMessage name="title" component="div" className="text-red-500" />
+        </div>
+        <div className="mb-5">
+          <Field
+            as="textarea"
+            name="content"
+            placeholder="Body"
+            className="border border-gray-300 shadow p-3 w-full rounded mb-"
+          />
+          <ErrorMessage name="content" component="div" className="text-red-500" />
+        </div>
+        <button
+          type="submit"
+          className="block w-full bg-yellow-400 text-black font-bold p-4 rounded-lg hover:bg-yellow-500"
+        >
+          Update Note
+        </button>
+      </Form>
+    </Formik>
+  </div>
+);
 
-          <div className="mb-5">
-            <Field
-              as="textarea"
-              name="content"
-              placeholder="Body"
-              className="border border-gray-300 shadow p-3 w-full rounded mb-"
-            />
-            <ErrorMessage name="content" component="div" className="text-red-500" />
-          </div>
 
-          <button
-            type="submit"
-            className="block w-full bg-yellow-400 text-black font-bold p-4 rounded-lg hover:bg-yellow-500"
-          >
-            Update Note
-          </button>
-        </Form>
-      </Formik>
-    </div>
-  );
 };
-
 export default EditNote;
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
-import NoteReducer from './api/NoteSlice'
-
+import {NoteSlice} from './api/NoteSlice'
+import {setupListeners} from '@reduxjs/toolkit/dist/query'
 export const store = configureStore({
   reducer: {
-    notes: NoteReducer
+  [NoteSlice.reducerPath]:NoteSlice.reducer
 
-    }
+    }, 
+    middleware:(getDefaultMiddleware)=>  
+    getDefaultMiddleware().concat(NoteSlice.middleware)
 })
+setupListeners(store.dispatch)
+
